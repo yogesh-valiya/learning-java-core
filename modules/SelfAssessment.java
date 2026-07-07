5026199a156385277485dbe322c0f62f69cb32b0
Yogesh Valiya <yogesh.valiya@encora.com>
2026-07-07 18:09:59 +0530

    Update doc

--- modules/SelfAssessment.java
+++ modules/SelfAssessment.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class SelfAssessment {
     static interface IGreeter {
+        String defaultName = "Yogesh";
+
         String name();
+        void setName(String name);
 
         default String greet() {
             return "Hello " + name();
@@ -8,19 +15,29 @@ public class SelfAssessment {
     }
 
     static class Greeter implements IGreeter {
-        @java.lang.Override
-        public String name() {
-            return "Yogesh";
-        }
+        private String currentName;
 
         @java.lang.Override
-        public String greet() {
-            return "Welcome " + name() + '!';
+        public String name() {
+            return currentName != null ? this.currentName : IGreeter.defaultName;
+        }
+
+        @Override
+        public void setName(String name) {
+            this.currentName = name;
         }
     }
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.greet());
+        greeter.setName("Yogi");
+        System.out.println(greeter.greet());
+
+        List<String> names = Arrays.asList("Charlie", "Alpha", "Beta");
+
+        names.sort(Comparator.naturalOrder());
+
+        System.out.println(names); // Output: [Alpha, Beta, Charlie]
     }
 }
