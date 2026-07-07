816de17bc548584a860169fb0d7c7905adf56103
Yogesh Valiya <yogesh.valiya@encora.com>
2026-07-07 13:30:19 +0530

    Update doc

---
+++ modules/SelfAssesment.java
@@ -0,0 +1,10 @@
+public class SelfAssesment {
+    public static void main(String[] args) {
+        String s = "Hi There";
+        s = "Hellow";
+        String x = "Hi There";
+        s = "Hi There";
+
+        System.out.println()
+    }
+}
